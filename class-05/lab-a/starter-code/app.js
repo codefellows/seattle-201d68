5fa5f0f21092b2e365f8f640717505c05b1ddc83
Alex Angelico <alex.angelico@live.com>
2020-10-03 04:32:29 -0700

    created multiply()

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -29,7 +29,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var product = a * b;
+  var string = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
+  return [product, string];
 }
 
 // Here is the test for multiply(); uncomment it to run it
