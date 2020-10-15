43ecf00c41f6e2cf582caf16424679256d233f73
Nathan Rhead Cox <nrc4@hotmail.com>
2020-10-15 16:46:33 -0700

    First two of four hours in pair programming. Handing over the wheel.

--- class-14/lab/starter-code/js/app.js
+++ class-14/lab/starter-code/js/app.js
@@ -8,15 +8,22 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  new CartItem(product, quantity);
+
+  this.items.push(CartItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cartItems', JSON.stringify(this.items));
+
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  
+
 };
 
 var CartItem = function(product, quantity) {
@@ -24,12 +31,13 @@ var CartItem = function(product, quantity) {
   this.quantity = quantity;
 };
 
-// Product contructor.
+// Product constructor.
 var Product = function(filePath, name) {
   this.filePath = filePath;
   this.name = name;
   Product.allProducts.push(this);
 };
+
 Product.allProducts = [];
 
 function generateCatalog() {
--- class-14/lab/starter-code/js/catalog.js
+++ class-14/lab/starter-code/js/catalog.js
@@ -4,7 +4,7 @@
 
 // Set up an empty cart for use on this page.
 var cart = new Cart([]);
-
+console.log(cart);
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
@@ -12,17 +12,18 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
   for (var i in Product.allProducts) {
-
+    var optionElement = document.createElement('option');
+    optionElement.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(optionElement);
   }
-
 }
 
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
-
   // TODO: Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -35,8 +36,13 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
-  // TODO: get the quantity
+  var itemSelection = document.getElementById('items').value;
+ 
+ // TODO: get the quantity
+  var selectedQuantity = document.getElementById('quantity').value;
+
   // TODO: using those, add one item to the Cart
+  cart.addItem(itemSelection, selectedQuantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
